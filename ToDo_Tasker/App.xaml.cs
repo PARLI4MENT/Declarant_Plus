228ba60172e02752b58440354123d87b067c825f
PARLI4MENT <ilya6joker@gmail.com>
2024-03-13 15:11:10 +1000

    Add project files.

---
+++ ToDo_Tasker/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace ToDo_Tasker
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
---
+++ ToDo_Tasker_clr/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ToDo_Tasker_clr
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
