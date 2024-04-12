d1353c3e934c96acc0cb54209b9933407db719a4
PARLI4MENT <ilya6joker@gmail.com>
2024-04-13 00:13:05 +1000

    # Realise Base class "Parcels"

---
+++ practics1/Parcels.cs
@@ -0,0 +1,27 @@
+﻿
+namespace practics1
+{
+    public class Parcels
+    {
+        /// <summary>  Код => [0] </summary>
+        public string? Code {  get; set; }
+
+        /// <summary> Кол-во => [2] </summary>
+        public uint Count { get; set; }
+
+        /// <summary> Цена => [3] </summary>
+        public double Cost { get; set; }
+
+        /// <summary> Стоимость => [4] </summary>
+        public double Weight { get; set; }
+
+        /// <summary> Вес => [5] </summary>
+        public double Weight2 { get; set; }
+
+        /// <summary> Трек => [6] </summary>
+        public string? Track { get; set; }
+
+        public string GetOutData() => $"{this.Code}\t{this.Count}\t{this.Cost}\t{this.Weight}\t{this.Weight2}\t{this.Track}\n";
+
+    }
+}
--- practics1/Program.cs
+++ practics1/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using ClosedXML.Excel;
 
 
 namespace practics1
@@ -10,6 +11,25 @@ namespace practics1
 
         static void Main(string[] args)
         {
+            //var parcels = new List<Parcels>();
+
+
+
+            //using (var workbook = new XLWorkbook(strPath))
+            //{
+            //    var dataRow = workbook.Worksheet(1).RowsUsed();
+            //    int i = 0;
+            //    foreach (var row in dataRow)
+            //    {
+
+            //        parcels.Add(new Parcels()
+            //        {
+            //            Code = row.Cell(0).Value.ToString(),
+
+            //        });
+            //        i++;
+            //    }
+            //}
 
             Console.ReadKey();
         }
