997d5ba6204932ea5252559b1def54efc27d1147
PARLI4MENT <ilya6joker@gmail.com>
2024-04-12 23:13:37 +1000

    # Add new project # Practics 1

---
+++ practics1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+
+namespace practics1
+{
+
+    internal class Program
+    {
+        static readonly string strPath = "C:\\TaskParcels\\Parcels.xlsx";
+
+        static void Main(string[] args)
+        {
+
+            Console.ReadKey();
+        }
+    }
+}
