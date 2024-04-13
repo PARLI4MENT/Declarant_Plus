9d15e2e4ddbba3f79ac00ec09c2d1fa9fac92c93
PARLI4MENT <ilya6joker@gmail.com>
2024-04-13 23:23:37 +1000

    # Practics 1 # Read cells from Excel file

--- practics1/Parcels.cs
+++ practics1/Parcels.cs
@@ -3,25 +3,29 @@ namespace practics1
 {
     public class Parcels
     {
-        /// <summary>  Код => [0] </summary>
+        /// <summary>  Код => [1] </summary>
         public string? Code {  get; set; }
 
-        /// <summary> Кол-во => [2] </summary>
+        /// <summary> Кол-во => [3] </summary>
         public uint Count { get; set; }
 
-        /// <summary> Цена => [3] </summary>
-        public double Cost { get; set; }
+        /// <summary> Цена => [4] </summary>
+        public double Cost { get; set; } 
+        
+        /// <summary> Цена2 => [5] </summary>
+        public double Cost2 { get; set; }
 
-        /// <summary> Стоимость => [4] </summary>
+        /// <summary> Вес => [6] </summary>
         public double Weight { get; set; }
 
-        /// <summary> Вес => [5] </summary>
+        /// <summary> Вес2 => [7] </summary>
         public double Weight2 { get; set; }
 
-        /// <summary> Трек => [6] </summary>
+        /// <summary> Трек => [8] </summary>
         public string? Track { get; set; }
 
-        public string GetOutData() => $"{this.Code}\t{this.Count}\t{this.Cost}\t{this.Weight}\t{this.Weight2}\t{this.Track}\n";
+        public string? GetOutData() =>
+            $"{this.Code}\t{this.Count}\t{this.Cost}\t{this.Cost2}\t{this.Weight}\t{this.Weight2}\t{this.Track}\n";
 
     }
 }
--- practics1/Program.cs
+++ practics1/Program.cs
@@ -16,30 +16,34 @@ namespace practics1
 
             using (var workbook = new XLWorkbook(strPath))
             {
-
                 //workbook.ShowZeros = false;
-                var dataRow = workbook.Worksheet(1).SetShowRowColHeaders(false).RowsUsed();
+                var dataRow = workbook.Worksheet(1).RowsUsed().Skip(1);
 
                 int i = 0;
                 foreach (var row in dataRow)
                 {
-                    Console.WriteLine($"{row.Cell(1).Value.ToString()}\t{row.Cell(3).Value.ToString()}" +
-                        $"\t{row.Cell(4).Value.ToString()}\t{row.Cell(5).Value.ToString()}\t{row.Cell(6).Value.ToString()}" +
-                        $"\t{row.Cell(7).Value.ToString()} \t{row.Cell(8).Value.ToString()}");
-                    //parcels.Add(new Parcels()
-                    //{
-                    //    Code = row.Cell(1).Value.ToString(),
-                    //    Count = Convert.ToUInt16(row.Cell(3).Value),
-                    //    Cost = Convert.ToDouble(row.Cell(4).Value),
-                    //    Weight = Convert.ToDouble(row.Cell(5).Value),
-                    //    Weight2 = Convert.ToDouble(row.Cell(6).Value),
-                    //    Track = row.Cell(7).Value.ToString().ToUpper()
-                    //});
-                    //Console.WriteLine($"{i} => {parcels[i].Code}\t{parcels[i].Count}\t" +
-                    //    $"{parcels[i].Cost}\t{parcels[i].Weight}\t{parcels[i].Weight2}\t" +
-                    //    $"{parcels[i].Track}\n");
+                    Debug.WriteLine($"{row.Cell(1).Value.ToString()}\t" +
+                        $"{row.Cell(3).Value.ToString()}" +
+                        $"\t{row.Cell(4).Value.ToString()}\t" +
+                        $"{row.Cell(5).Value.ToString()}\t" +
+                        $"{row.Cell(6).Value.ToString()}" +
+                        $"\t{row.Cell(7).Value.ToString()}\t" +
+                        $"{row.Cell(8).Value.ToString()}");
+
+                    parcels.Add(new Parcels()
+                    {
+                        Code = row.Cell(1).Value.ToString(),
+                        Count = Convert.ToUInt32(row.Cell(3).Value.ToString()),
+                        Cost = Convert.ToDouble(row.Cell(4).Value.ToString()),
+                        Cost2 = Convert.ToDouble(row.Cell(5).Value.ToString()),
+                        Weight = Convert.ToDouble(row.Cell(6).Value.ToString()),
+                        Weight2 = Convert.ToDouble(row.Cell(7).Value.ToString()),
+                        Track = row.Cell(8).Value.ToString().ToUpper()
+                    });
                     i++;
                 }
+
+                Console.ReadLine();
             }
 
             Console.ReadKey();
